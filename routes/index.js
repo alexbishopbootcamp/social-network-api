bc569294ce7d066740481725f9a5d93c05269f50
Alex <github@alexbi.shop>
2023-10-17 17:16:06 +0900

    Thought routes

--- routes/index.js
+++ routes/index.js
@@ -125,5 +125,116 @@ router.delete('/api/users/:userId/friends/:friendId', (req, res) => {
 });
 
 
+// GET all thoughts
+router.get('/api/thoughts', (req, res) => {
+  console.log("Got a request for thoughts")
+  Thought.find({})
+    .populate({
+      path: 'reactions',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbThoughtData => res.json(dbThoughtData))
+    .catch(err => res.status(400).json(err));
+});
+
+// GET thought by ID
+router.get('/api/thoughts/:id', (req, res) => {
+  console.log("Got a request for a thought by ID")
+  Thought.findOne({ _id: req.params.id })
+    .populate({
+      path: 'reactions',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought found with this ID!' });
+        return;
+      }
+      res.json(dbThoughtData)
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// POST a new thought
+router.post('/api/thoughts', (req, res) => {
+  console.log("Got a request to create a new thought")
+  const newThought = {
+    thoughtText: req.body.thoughtText,
+    username: req.body.username
+  };
+  Thought.create(newThought)
+    .then(dbThoughtData => {
+      // Add thought to user's thoughts array
+      return User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: dbThoughtData._id } },
+        { new: true }
+      );
+    })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id!" });
+        return;
+      }
+      res.json(dbUserData)
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// PUT update a thought by ID
+router.put('/api/thoughts/:id', (req, res) => {
+  console.log("Got a request to update a thought by ID")
+  Thought.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      thoughtText: req.body.thoughtText,
+      username: req.body.username
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  )
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: 'No thought found with this ID!' });
+        return;
+      }
+      res.json(dbThoughtData)
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// DELETE a thought by ID
+router.delete('/api/thoughts/:id', (req, res) => {
+  console.log("Got a request to delete a thought by ID");
+  Thought.findOneAndDelete({ _id: req.params.id })
+    .then(dbThoughtData => {
+      if (!dbThoughtData) {
+        res.status(404).json({ message: "No thought found with this ID!" });
+        return;
+      }
+      // Remove thought from user's thoughts array
+      return User.findOneAndUpdate(
+        { username: dbThoughtData.username },
+        { $pull: { thoughts: req.params.id } },
+        { new: true }
+      );
+    })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this ID!" });
+        return;
+      }
+      res.json({ message: "Thought and associated reactions deleted!" });
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+
+
+
 
 module.exports = router;
