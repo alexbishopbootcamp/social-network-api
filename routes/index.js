6f13bd00b73af02e5e5b4ce5cbaab2325847cd8a
Alex <github@alexbi.shop>
2023-10-17 16:41:18 +0900

    User routes

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User, Thought } = require('../models');
 
 // GET all users
-router.get('/users', (req, res) => {
+router.get('/api/users', (req, res) => {
   console.log("Got a request for users")
   User.find({})
     .populate({
@@ -14,6 +14,77 @@ router.get('/users', (req, res) => {
     .catch(err => res.status(400).json(err));
 });
 
+// GET user by ID
+router.get('/api/users/:id', (req, res) => {
+  console.log("Got a request for a user by ID")
+  User.findOne({ _id: req.params.id })
+    .populate({
+      path: 'thoughts',
+      select: '-__v'
+    })
+    .select('-__v')
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this ID!' });
+        return;
+      }
+      res.json(dbUserData)
+    })
+    .catch(err => res.status(400).json(err));
+});
 
+// POST a new user
+router.post('/api/users', (req, res) => {
+  console.log("Got a request to create a new user")
+  const newUser = {
+    username: req.body.username,
+    email: req.body.email
+  };
+  User.create(newUser)
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => res.status(400).json(err));
+});
+
+// PUT update a user by ID
+router.put('/api/users/:id', (req, res) => {
+  console.log("Got a request to update a user by ID")
+  User.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      username: req.body.username,
+      email: req.body.email
+    },
+    {
+      new: true,
+      runValidators: true
+    }
+  )
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'No user found with this ID!' });
+        return;
+      }
+      res.json(dbUserData)
+    })
+    .catch(err => res.status(400).json(err));
+});
+
+// DELETE a user by ID
+router.delete('/api/users/:id', (req, res) => {
+  console.log("Got a request to delete a user by ID");
+  User.findOneAndDelete({ _id: req.params.id })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this ID!" });
+        return;
+      }
+      // Remove all thoughts associated with this user
+      return Thought.deleteMany({ username: dbUserData.username });
+    })
+    .then(() => {
+      res.json({ message: "User and associated thoughts deleted!" });
+    })
+    .catch(err => res.status(400).json(err));
+});
 
 module.exports = router;
